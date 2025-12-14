dca1ff5e292e23a0235b4e1d63c7eba2ce66a1de
nadirhussainnn <nadirhussaintumrani@gmail.com>
2025-12-14 18:34:00 +0100

    update: created authentication schemas, migrated, added delete feature to delete product

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ruby-e-com
 This app is created as part of the course work "Frameworks for Rapid Application development" course.
+
+## How to run?
+- Install ruby >= 3.4.7
+- Install rails >= 8.1.1
+- Then clone this repo, go inside store folder
+- Run `bundle install` to install all gems required for this project
+- Run `bin/rails server` to start server
+- Visit `http://127.0.0.1:3000`
+
+# To activate rails console
+- `bin/rails console`
+
+# Creating tables and running migrations
+- `bin/rails generate model Product name:string`
+- `bin/rails db:migrate`
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,31 +1,34 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: %i[show edit update destroy]
+
   def index
     @products = Product.all
   end
 
   def show
-    @product = Product.find(params[:id])
   end
 
+  # Creates form for new product on GET /products/new request
   def new
     @product = Product.new
   end
 
+  # This is required when the /products/new form is submitted
    def create
     @product = Product.new(product_params)
     if @product.save
       redirect_to @product
     else
-      render :new, status: :unprocessable_entity
+      render :new, status: :unprocessable_entity  # we set 422 status code for validation errors
     end
   end
 
+  # This action renders the edit form on url /products/:id/edit
   def edit
-    @product = Product.find(params[:id])
   end
 
+  # This action updates the product when the edit form is submitted
   def update
-    @product = Product.find(params[:id])
     if @product.update(product_params)
       redirect_to @product
     else
@@ -33,7 +36,20 @@ class ProductsController < ApplicationController
     end
   end
   
+  # This action deletes the product
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+
+  # This is required to filter the parameters from the form submission for edit. Required for security.
+  # Use strong parameters to allow only the permitted attributes.
   private
+    def set_product
+      @product = Product.find(params[:id])
+    end
+
     def product_params
       params.expect(product: [ :name ])
     end
