021a3eb07c3cc1abb8a2ed2096d451b4d9aee8d2
nadirhussainnn <nadirhussaintumrani@gmail.com>
2025-12-15 14:06:32 +0100

    fix: corrected formatting issues with rubocop

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,19 +58,17 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { 
-    host: "ruby-e-com.onrender.com", protocol: "https" 
-  }
+  config.action_mailer.default_url_options = { host: "ruby-e-com.onrender.com", protocol: "https" }
 
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    address: ENV['SMTP_ADDRESS'],      
-    port: ENV['SMTP_PORT'],            
-    domain: ENV['SMTP_DOMAIN'],        
-    user_name: ENV['SMTP_USERNAME'],
-    password: ENV['SMTP_PASSWORD'],
-    authentication: 'plain',
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: "plain",
     enable_starttls_auto: true
   }
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
